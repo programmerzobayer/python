3e5a7637cba5dff7b23e09bbf6db0d93ea29ac40
programmerzobayer <programmerzobayer@gmail.com>
2022-12-01 01:04:30 +0600

    Upload for-loop.py

---
+++ for-loop.py
@@ -0,0 +1,11 @@
+for evennum in range(10):
+   if evennum % 2 == 0:
+    print(evennum)
+
+print("Odd Number Start")
+for oddnum in range(10):
+  if oddnum % 2 != 0:
+    print(oddnum)
+
+for name in range(5):
+    print("jubeyer")
