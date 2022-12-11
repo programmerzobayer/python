ff776931ef13865804dde280bb9969ec78d79ab8
programmerzobayer <programmerzobayer@gmail.com>
2022-12-11 14:25:26 +0600

    Upload function.py

---
+++ function.py
@@ -0,0 +1,17 @@
+def multiply(num):
+    print(num * 1)
+    print(num * 2)
+    print(num * 3)
+    print(num * 4)
+    print(num * 5)
+    print(num * 6)
+    print(num * 7)
+    print(num * 8)
+    print(num * 9)
+    print(num * 10)
+
+ 
+ 
+num = int(input("Enter a Number :"))
+multiply(num)
+
